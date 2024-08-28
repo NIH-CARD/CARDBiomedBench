1aa945b4068f5681ac7a35ea7ca8c6f0442e2442
oween12 <owen.bianchi12@gmail.com>
2024-08-28 15:26:03 -0400

    added sorting of models

--- scripts/graphs_runner.py
+++ scripts/graphs_runner.py
@@ -1,7 +1,7 @@
 import argparse
 from scripts import MODELS_DICT, METRICS_DICT
 from scripts.scripts_utils import load_dataset
-from scripts.generate_graphs.boxplot import plot_BioScore_boxplot, plot_metric_boxplot
+from scripts.generate_graphs.boxplot import plot_metric_boxplot
 from scripts.generate_graphs.heatmap import plot_metric_heatmap, plot_idk_heatmap
 from scripts.generate_graphs.table import create_performance_table, style_dataframe
 from scripts.generate_graphs.generate_graphs_utils import merge_model_responses
@@ -24,7 +24,7 @@ def main():
     
     metrics_list = []
     if "BioScore" in METRICS_DICT:
-        plot_BioScore_boxplot(data, "BioScore", MODELS_DICT, "BioScore Boxplot", "results/")
+        plot_metric_boxplot(data, "BioScore", MODELS_DICT, "BioScore Boxplot", "results/")
         plot_metric_heatmap(data, "BioScore", MODELS_DICT, "bio_category", "BioScore Bio Heatmap", "results/")
         plot_idk_heatmap(data, "BioScore", MODELS_DICT, "bio_category", "IDK Bio Heatmap", "results/")
         metrics_list += ["BioScore"]
