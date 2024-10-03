b7750257f6f31d22f6f1c5f9d5771779a5f09001
oween12 <owen.bianchi12@gmail.com>
2024-10-03 14:26:20 -0400

    cmd line arg for single model

--- scripts/responses_runner.py
+++ scripts/responses_runner.py
@@ -9,11 +9,13 @@ def main():
     parser.add_argument('--qa_path', type=str, required=True, help='Path to the QA CSV file')
     parser.add_argument('--res_dir', type=str, required=True, help='Directory to save the response CSV files')
     parser.add_argument('--template', type=bool, default=False, help="Whether to run template-based sampling")
+    parser.add_argument('--model_name', type=str, default="none", help="Specify a single model to run (optional)")
     args = parser.parse_args()
 
     qa_path = args.qa_path
     res_dir = args.res_dir
     template_flag = args.template
+    model_name = args.model_name
 
     data = load_dataset(qa_path)
     if data.empty:
@@ -27,15 +29,22 @@ def main():
         except ValueError as e:
             print(f"Error during sampling: {e}")
             return
-    
-    # TODO DELETE
-    data = data[['uuid', 'question', 'answer']]
-    data.dropna(inplace=True)
-    # TODO DELETE
 
-    print("## Getting model responses")
-    data = get_all_model_responses(data, model_dict=MODELS_DICT, res_dir=res_dir)
-    print(f"## Responses collected and saved to {res_dir}")
+    selected_models_dict = MODELS_DICT.copy()
+
+    # Filter the model dictionary if a model name is provided
+    if model_name != "none":
+        if model_name in selected_models_dict:
+            print(f"## Running only for model: {model_name} ##")
+            # Keep only the selected model in the MODELS_DICT
+            selected_models_dict = {model_name: selected_models_dict[model_name]}
+        else:
+            print(f"Model {model_name} not found in configuration. Exiting.")
+            return
+
+    print(f"## Getting model responses on {len(data)} Q/A ##")
+    data = get_all_model_responses(data, model_dict=selected_models_dict, res_dir=res_dir)
+    print(f"## Responses collected and saved to {res_dir} ##")
 
 if __name__ == "__main__":
     main()
