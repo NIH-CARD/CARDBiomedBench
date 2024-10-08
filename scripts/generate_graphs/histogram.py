8c99955a999c8d0f477fc7ea12b92e7fe918e63b
oween12 <owen.bianchi12@gmail.com>
2024-10-08 15:16:06 -0400

    filtering outliers

--- scripts/generate_graphs/histogram.py
+++ scripts/generate_graphs/histogram.py
@@ -4,22 +4,34 @@ import math
 import matplotlib.pyplot as plt
 
 def plot_token_histograms(data: pd.DataFrame, text_col: str, color: str, title: str, save_path: str):
-    """Create a histogram to visualize token counts for a given text column, showing frequency as a percentage."""
+    """Create a histogram to visualize token counts for a given text column, showing frequency as a percentage, and filter outliers."""
     sns.set_style("whitegrid")
     sns.set_context("talk")
     
     token_col = f'{text_col}_token_count'
 
-    # Print median and sum token count
-    median_token_count = data[token_col].median()
+    # Calculate Q1 (25th percentile), Q3 (75th percentile), and Interquartile range
+    Q1 = data[token_col].quantile(0.25)
+    Q3 = data[token_col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Define lower and upper bounds for filtering outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Filter the data to exclude outliers
+    filtered_data = data[(data[token_col] >= lower_bound) & (data[token_col] <= upper_bound)]
+
+    # Print median and sum token count of the filtered data
+    median_token_count = filtered_data[token_col].median()
 
     # Determine the maximum token count and calculate the step size
-    max_token_count = data[token_col].max()
+    max_token_count = filtered_data[token_col].max()
     step_size = math.ceil(max_token_count / 10 / 10) * 10
 
     # Plot the histogram with percentage on y-axis
     plt.figure(figsize=(10, 4), dpi=100)
-    sns.histplot(data[token_col], color=color, kde=True, stat='percent')
+    sns.histplot(filtered_data[token_col], color=color, kde=True, stat='percent')
     
     # Add a vertical dashed line at the median
     plt.axvline(median_token_count, color='black', linestyle='--', label=f'Median: {median_token_count:.0f}')
