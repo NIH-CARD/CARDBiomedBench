e4c45ca7c8e77e8fa10829d4ef27f9adef49e1d0
oween12 <owen.bianchi12@gmail.com>
2024-11-10 16:24:07 -0500

    bang

--- scripts/scripts_utils.py
+++ scripts/scripts_utils.py
@@ -1,26 +1,5 @@
 import os
-import yaml
 import pandas as pd
-
-def load_config():
-    """
-    Load the configuration from the config.yaml file.
-
-    Returns:
-    - dict: Configuration dictionary.
-    """
-    try:
-        config_path = os.path.join(os.path.dirname(__file__), '../configs/config.yaml')
-        with open(config_path, 'r') as f:
-            config = yaml.safe_load(f)
-        return config
-    except FileNotFoundError:
-        print(f"Configuration file not found at {config_path}")
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file: {e}")
-    except Exception as e:
-        print(f"Error loading configuration file: {e}")
-    return {}
 
 def load_dataset(filepath: str) -> pd.DataFrame:
     """
