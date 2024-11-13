5566e3813f585388a0be084ddf99a4552ef2eb1d
oween12 <owen.bianchi12@gmail.com>
2024-11-13 15:17:21 -0500

    unneeded

--- scripts/metrics_runner.py
+++ scripts/metrics_runner.py
@@ -1,11 +1,12 @@
 import argparse
-from scripts import MODELS_DICT, METRICS_DICT, GRADING_MODEL
+from scripts import MODELS_DICT, METRICS_DICT
 from scripts.compute_metrics.BleuRougeBert import get_all_model_BLEU_ROUGE_BERT
 from scripts.compute_metrics.BioScore import get_all_model_BioScore
 
 def main():
     parser = argparse.ArgumentParser(description="Grade responses on the QA benchmark.")
     parser.add_argument('--res_dir', type=str, required=True, help='Directory to the responses CSV files')
+    parser.add_argyment('--')
     args = parser.parse_args()
 
     res_dir = args.res_dir
